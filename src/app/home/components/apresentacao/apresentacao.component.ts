7fa51d9de92d5c16adbf2bca369730b0435f3861
CaduLopes87 <edu.lopesbezerra@gmail.com>
2024-11-17 15:58:11 -0300

    FEAT: Cria sessão de apresentação

--- src/app/home/components/apresentacao/apresentacao.component.ts
+++ src/app/home/components/apresentacao/apresentacao.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HomeService } from '../../services/home.service';
 
 @Component({
   selector: 'app-apresentacao',
   templateUrl: './apresentacao.component.html',
-  styleUrl: './apresentacao.component.scss'
+  styleUrl: './apresentacao.component.scss',
 })
 export class ApresentacaoComponent {
+  constructor(private homeService: HomeService) {}
 
+  animarIcone(event: any, animacaoClasse: string, redeSocialLink: string = '') {
+    this.homeService.animarIcone(event, animacaoClasse, redeSocialLink);
+  }
+
+  animarBotao(
+    event: any,
+    botaoId: string,
+    animacaoClasse: string,
+    link: string = ''
+  ) {
+    this.homeService.animarBotao(event, botaoId, animacaoClasse, link);
+  }
 }
