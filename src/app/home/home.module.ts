156020b29bd724fdf9e911ce374a315edcec5b50
CaduLopes87 <edu.lopesbezerra@gmail.com>
2024-11-19 22:38:36 -0300

    FEAT: Cria componente de projetos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,18 @@ import { HeaderComponent } from './components/header/header.component';
 import { ApresentacaoComponent } from './components/apresentacao/apresentacao.component';
 import { SobreComponent } from './components/sobre/sobre.component';
 import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
 
 @NgModule({
-  declarations: [BackgroungParticlesComponent, HomeComponent, HeaderComponent, ApresentacaoComponent, SobreComponent, TecnologiasComponent],
+  declarations: [
+    BackgroungParticlesComponent,
+    HomeComponent,
+    HeaderComponent,
+    ApresentacaoComponent,
+    SobreComponent,
+    TecnologiasComponent,
+    ProjetosComponent,
+  ],
   imports: [
     CommonModule,
 
