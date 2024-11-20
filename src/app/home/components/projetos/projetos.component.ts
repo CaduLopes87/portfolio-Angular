1f1e0082b97809c71b4ce4bd5d9ac275f09e5026
CaduLopes87 <edu.lopesbezerra@gmail.com>
2024-11-19 23:58:32 -0300

    FEAT: Adiciona novo projeto à lista

--- src/app/home/components/projetos/projetos.component.ts
+++ src/app/home/components/projetos/projetos.component.ts
@@ -17,19 +17,12 @@ export class ProjetosComponent {
         projeto_Link: 'https://contabilizador-de-rifas.vercel.app/',
       },
       {
-        nome: 'Contabilizador de Rifas',
-        src: '../../../../assets/contabilizador-backdrop.png',
-        alt: 'Imagem ilustrativa do site contabilizador de rifas',
-        github_Link: 'https://github.com/CaduLopes87/contabilizador-de-rifas',
-        projeto_Link: 'https://contabilizador-de-rifas.vercel.app/',
-      },
-      {
-        nome: 'Contabilizador de Rifas',
-        src: '../../../../assets/contabilizador-backdrop.png',
-        alt: 'Imagem ilustrativa do site contabilizador de rifas',
-        github_Link: 'https://github.com/CaduLopes87/contabilizador-de-rifas',
-        projeto_Link: 'https://contabilizador-de-rifas.vercel.app/',
-      },
+        nome: 'Onirica Psique',
+        src: '../../../../assets/oniricapsique-backdrop.png',
+        alt: 'Imagem ilustrativa do site Onirica Psique',
+        github_Link: 'https://github.com/CaduLopes87/tarot-landpage',
+        projeto_Link: 'https://oniricapsique.vercel.app/',
+      }
     ],
     pessoais: [
       {
